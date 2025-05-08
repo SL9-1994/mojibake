6a27b47c38a59fabf5ca3b85b426bd18243fdf0b
yurari <320sl91994@gmail.com>
2025-05-08 23:12:16 +0900

    feat:✨ テープ実装を行うMemory構造体を新規作成

---
+++ src/interpreter/memory.rs
@@ -0,0 +1,11 @@
+pub struct Memory {}
+
+// impl Memory {
+//     pub fn new() -> Self;
+//     pub fn inc(&mut self);
+//     pub fn dec(&mut self);
+//     pub fn move_right(&mut self);
+//     pub fn move_left(&mut self);
+//     pub fn get(&self) -> u8;
+//     pub fn set(&mut self, val: u8);
+// }
