247e378be8592203c8aa0bdb76ea90ca3b91bd8a
yurari <320sl91994@gmail.com>
2025-05-08 23:11:00 +0900

    feat:✨ lib.rsを追加し、エラー処理とインタプリターモジュールを定義

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub mod error;
+// run以外のモジュールは開発時のみ公開 -> 本番環境では秘匿
+pub mod interpreter {
+    pub mod lexer;
+    pub mod memory;
+    pub mod parser;
+    pub mod run;
+}
+
+pub use error::MtError;
+pub use interpreter::run::execute;
