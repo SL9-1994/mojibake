cd6479ca6c637e95d9615f876ea8b36cf5bf8faa
yurari <320sl91994@gmail.com>
2025-05-10 15:32:42 +0900

    feat:✨ MJBKインタプリタの実行バイナリを実装し、コマンドライン引数から.mjbkファイルを読み込む機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+};
+
+use clap::Parser;
+use mojibake::{
+    MjbkError,
+    interpreter::{lexer::tokenize, memory::Memory, parser::parse, run::run_with_io},
+};
+
+// Brainfuck 派生言語 MJBK の実行バイナリ
+#[derive(Debug, PartialEq, Eq, Clone, Parser)]
+#[clap(author, version, about = "Minimal Japanese Mojibake Brainfuck-style interpreter type language.", long_about = None)]
+struct Cli {
+    /// 実行する .mjbk ファイルへのパス
+    #[arg(value_name = "FILE")]
+    file: std::path::PathBuf,
+}
+
+fn main() -> Result<(), MjbkError> {
+    let cli = Cli::parse();
+
+    let file = File::open(&cli.file).map_err(|e| MjbkError::Io(e))?;
+    let mut reader = BufReader::new(file);
+    let mut code = String::new();
+    reader.read_to_string(&mut code)?;
+
+    let tokens = tokenize(&code)?;
+    let ast = parse(&tokens)?;
+
+    let mut std_output = std::io::stdout().lock();
+    let mut std_input = std::io::stdin().lock();
+    let mut memory = Memory::new();
+
+    run_with_io(&ast, &mut memory, &mut std_output, &mut std_input)?;
+
+    Ok(())
 }
